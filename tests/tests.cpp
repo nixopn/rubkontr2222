0e6839ff7a7c667b08b9df2d138d604dbc9477bd
nixopn <144889708+nixopn@users.noreply.github.com>
2024-05-19 14:26:29 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,1 +1,38 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
+#include "ConcreteHandlerA.h"
+#include "ConcreteHandlerB.h"
+
+
+class MockHandler : public IHandler {
+public:
+    MOCK_CONST_METHOD0(canhandle, bool());
+    MOCK_METHOD1(setHandler, void(IHandler* handler));
+    MOCK_CONST_METHOD0(handleRequest, void());
+};
+
+TEST(HandlerTest, CanHandleTest) {
+    MockHandler handler;
+    EXPECT_CALL(handler, canhandle()).WillOnce(testing::Return(true));
+
+    EXPECT_TRUE(handler.canhandle());
+}
+
+TEST(HandlerTest, SetHandlerTest) {
+    MockHandler handler1, handler2;
+    EXPECT_CALL(handler1, setHandler(&handler2));
+
+    handler1.setHandler(&handler2);
+
+}
+
+TEST(HandlerTest, HandleRequestTest) {
+    MockHandler handler1, handler2;
+    
+    EXPECT_CALL(handler1, handleRequest()).Times(1);
+    EXPECT_CALL(handler2, handleRequest()).Times(1);
+
+    handler1.setHandler(&handler2);
+    handler1.handleRequest();
+}
